d3bbf3f8c930f72f4b882a164850cf664fcfc152
Dawid Dziurla <dawidd0811@gmail.com>
2021-01-30 14:04:14 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -35,6 +35,7 @@
 <style>
         body {
                 background-color: #e6fcfd;
+	        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
         }
 
 	main {
